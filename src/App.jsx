17de40c1bcb0586dff08b15d5ba3ae7ab0304aad
Shaiful191 <saifulislamridoy1999@gmail.com>
2024-12-21 12:58:25 +0600

    completed chepter 10

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Home from './components/Home'
+import Navber from './components/Navber'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Navber />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
