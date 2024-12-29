a99e6119e57ca20aa8fc77f928ed8363cdfd75d3
Shaiful191 <saifulislamridoy1999@gmail.com>
2024-12-29 22:13:05 +0600

    change useRef to useState()

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,29 +1,19 @@
-import React, { useRef, useState } from 'react'
+
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-const Create = () => { 
+const Create = () => {
 
-  const titleElement = useRef('');
-  const bodyElement = useRef('');
-  const authorElement = useRef('shaiful');
-
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('shaiful');
 
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const title = titleElement.current.value;
-    const body = bodyElement.current.value;
-    const author = authorElement.current.value;
-
-
     const blog = { title, body, author };
-
-    titleElement.current.value = '';
-    bodyElement.current.value = '';
-    authorElement.current.value = 'shaiful';
 
     setIsLoading(true);
 
@@ -43,27 +33,29 @@ const Create = () => {
     <div className='create'>
       <h2>Add A New Blog</h2>
       <form onSubmit={handleSubmit}>
-     
         <label>Blog title:</label>
         <input
           type="text"
           required
-          ref={titleElement}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog body:</label>
         <textarea
           required
-          ref={bodyElement}
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
         <select
-          ref={authorElement}>
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}>
           <option value="shaiful">shaiful</option>
           <option value="abdulla">abdulla</option>
         </select>
 
 
-        {!isLoading && <button >Add Blog</button>}
+        {!isLoading && <button>Add Blog</button>}
         {isLoading && <button disabled>Adding...</button>}
 
       </form>
@@ -71,4 +63,7 @@ const Create = () => {
   )
 }
 
-export default Create
+export default Create 
+
+
+
